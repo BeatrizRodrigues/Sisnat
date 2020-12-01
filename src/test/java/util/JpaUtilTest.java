90a6990c42d6a0756eb72b3e5b00efb8b6431014
Beatriz Rodrigues <beeatrizz29@gmail.com>
2020-12-01 12:28:43 -0300

    importacao inicial

---
+++ src/test/java/util/JpaUtilTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import javax.persistence.EntityManager;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author beeat
+ */
+public class JpaUtilTest {
+    
+    public JpaUtilTest() {
+    }
+
+    @org.junit.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+    
+
+    @org.junit.Test
+    public void testaConexao() {
+        EntityManager em = JpaUtil.getEntityManager();
+        assertNotNull(em);
+    }
+    
+}
